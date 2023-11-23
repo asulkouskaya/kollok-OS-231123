3140448870a15e9ab5496f511313bcd6c149b49b
Helia <124560143+asulkouskaya@users.noreply.github.com>
2023-11-23 12:29:57 +0300

    Update Kollok2_OS.cpp
    update main

--- Kollok2_OS.cpp
+++ Kollok2_OS.cpp
@@ -1,4 +1,4 @@
-﻿#include <iostream>
+#include <iostream>
 #include <fstream>
 #include <vector>
 #include <thread>
@@ -7,11 +7,11 @@
 #include <cmath>
 #include <cassert>
 #include <string>
+#include <sstream>
 
 using namespace std;
 
-class Operation
-{
+class Operation {
 public:
     virtual ~Operation() = default;
     virtual double execute(const vector<double>& numbers) const = 0;
@@ -50,186 +50,84 @@ public:
     }
 };
 
-
-class FileProcessor {
-private:
-    string directory;
-    int threadsCount;
-    mutex mutex;
-    queue<string> inputFiles;
-    double totalResult;
-
-    void processFile() {
-        string inputFile = getNextFile();
-
-        while (!inputFile.empty()) {
-            ifstream in(directory + "/" + inputFile);
-            if (in.is_open()) {
-                int action;
-                in >> action;
-
-                Operation* operation = nullptr;
-                switch (action) {
-                    case 1:
-                      operation = new Addition();
-                        break;
-                    case 2:
-                        operation = new Multiplication();
-                        break;
-                    case 3:
-                        operation = new SumOfSquares();
-                        break;
-                    default:
-                        throw runtime_error("Неверный номер действия в файле " + inputFile);
-                }
-
-                vector<double> numbers;
-                double number;
-                while (in >> number) {
-                    numbers.push_back(number);
-                }
-
-                in.close();
-                double result = operation->execute(numbers);
-                delete operation;
-                updateTotalResult(result);
-            }
-            else
-            {
-                throw runtime_error("Не удалось открыть файл " + inputFile + " для чтения");
-            }
-
-            inputFile = getNextFile();
+void processFile(const std::string& filename, const Operation& action, double& result, std::mutex& mutex) {
+    ifstream file(filename);
+    if (file.is_open()) {
+        vector<double> numbers;
+        double num;
+        while (file >> num) {
+            numbers.push_back(num);
         }
-    }
+        file.close();
 
-    string getNextFile() {
+        double partialResult = action.execute(numbers);
+
         lock_guard<std::mutex> lock(mutex);
-
-        if (inputFiles.empty()) {
-            return "";
-        }
-
-        string inputFile = inputFiles.front();
-        inputFiles.pop();
-        return inputFile;
-    }
-
-    void updateTotalResult(double result) {
-        lock_guard<std::mutex> lock(mutex);
-        totalResult += result;
-    }
-
-public:
-    FileProcessor(const string& directory, int threadsCount) {
-        if (directory.empty()) {
-            throw invalid_argument("Директория не может быть пустой");
-        }
-
-        if (threadsCount <= 0) {
-            throw invalid_argument("Количество потоков должно быть положительным");
-        }
-
-        this->directory = directory;
-        this->threadsCount = threadsCount;
-        totalResult = 0;
-    }
-
-    void run() {
-        for (int i = 1; i <= 10; i++) {
-            inputFiles.push("in_" + to_string(i) + ".dat");
-        }
-
-        vector<std::thread> threads;
-
-        for (int i = 0; i < threadsCount; i++) {
-            threads.emplace_back(&FileProcessor::processFile, this);
-        }
-
-        for (std::thread& thread : threads) {
-            if (thread.joinable()) {
-                thread.join();
-            }
-        }
-
-        ofstream out(directory + "/out.dat");
-        if (out.is_open()) {
-            out << totalResult << endl;
-            out.close();
-        }
-        else {
-            throw runtime_error("Не удалось открыть файл out.dat для записи");
-        }
-    }
-};
-
-void checkResult(const string& directory) {
-    ifstream in(directory + "/out.dat");
-    if (in.is_open()) {
-        double totalResult;
-        in >> totalResult;
-        in.close();
-        double expected = 0;
-        for (int i = 1; i <= 100; i++) {
-            ifstream in(directory + "/in_" + to_string(i) + ".dat");
-            if (in.is_open()) {
-                int action;
-                in >> action;
-                Operation* operation = nullptr;
-                switch (action) {
-                    case 1:
-                        operation = new Addition();
-                        break;
-                    case 2:
-                        operation = new Multiplication();
-                        break;
-                    case 3:
-                        operation = new SumOfSquares();
-                        break;
-                    default:
-                        throw runtime_error("Неверный номер действия в файле in_" + to_string(i) + ".dat");
-                }
-
-                vector<double> numbers;
-                double number;
-                while (in >> number) {
-                    numbers.push_back(number);
-                }
-
-                in.close();
-                double result = operation->execute(numbers);
-                delete operation;
-                expected += result;
-            }
-            else {
-                throw runtime_error("Не удалось открыть файл in_" + to_string(i) + ".dat для чтения");
-            }
-        }
-
-        double epsilon = 1e-6;
-        assert(abs(totalResult - expected) < epsilon);
+        result += partialResult;
     }
     else {
-        throw runtime_error("Не удалось открыть файл out.dat для чтения");
+        cerr << "Error opening file: " << filename << std::endl;
     }
 }
 
 int main(int argc, char* argv[]) {
-
-    setlocale(LC_ALL, "RUSSIAN");
-
     if (argc != 3) {
-        cerr << "Неверное количество аргументов" << endl;
+        cerr << "Usage: " << argv[0] << " <directory_path> <num_threads>" << std::endl;
         return 1;
     }
 
-    string directory = argv[1];
-    int threadsCount = stoi(argv[2]);
+    std::string directoryPath = argv[1];
+    int numThreads = stoi(argv[2]);
 
-    FileProcessor fileProcessor(directory, threadsCount);
-    fileProcessor.run();
-    checkResult(directory);
-    cout << "Приложение успешно выполнено" << endl;
+    Addition addition;
+    Multiplication multiplication;
+    SumOfSquares squareSum;
+
+    vector<std::thread> threads;
+    double totalResult = 0;
+    std::mutex resultMutex;
+
+    for (int i = 1; i <= numThreads; ++i) {
+        std::string filename = directoryPath + "/in_" + std::to_string(i) + ".dat";
+        int actionType = i % 3 + 1;
+
+        const Operation* action = nullptr;
+
+        switch (actionType) {
+            case 1:
+                action = &addition;
+                break;
+            case 2:
+                action = &multiplication;
+                break;
+            case 3:
+                action = &squareSum;
+                break;
+        }
+
+        if (!action) {
+            cerr << "Error: Unsupported action type." << std::endl;
+            return 1;
+        }
+
+        threads.emplace_back(processFile, filename, std::ref(*action), std::ref(totalResult), std::ref(resultMutex));
+    }
+
+    for (std::thread& thread : threads) {
+        thread.join();
+    }
+
+    ofstream outFile(directoryPath + "/out.dat");
+    if (outFile.is_open()) {
+        stringstream resultStream;
+        resultStream << "Total Result: " << totalResult << std::endl;
+        outFile << resultStream.str();
+        outFile.close();
+    }
+    else {
+        std::cerr << "Error opening output file." << std::endl;
+        return 1;
+    }
 
     return 0;
 }
